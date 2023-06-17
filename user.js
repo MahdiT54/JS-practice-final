e9e60d469c8bdf82f0d9c7dc6229b0e23676e3e9
Mahdi T <mahditanzim2@gmail.com>
2023-06-16 23:27:23 -0400

    reusable code fix

--- user.js
+++ user.js
@@ -1,29 +1,38 @@
 const postListEl = document.querySelector('.post-list')
+const id = localStorage.getItem("id");
 
-function onSearchChange() {
-    console.log('3324')
+async function onSearchChange(event) {
+    const id = event.target.value
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+    const postsData = await posts.json();
+    postListEl.innerHTML = postsData.map((post) => `<div class="post">
+          <div class="post__title">
+            ${post.title}
+          </div>
+          <p class="post__body">
+            ${post.body}
+          </p>
+        </div>`
+      ).join('');
 }
 
-async function main() {
-  const id = localStorage.getItem("id");
-  const posts = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
-  );
+async function renderPosts() {
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
   const postsData = await posts.json();
-  console.log(postsData);
-
   postListEl.innerHTML = postsData.map(
-    (post) => `
-    <div class="post">
+    (post) => postHTML(post)).join('');
+}
+
+function postHTML(post) {
+    return `<div class="post">
       <div class="post__title">
         ${post.title}
       </div>
       <p class="post__body">
         ${post.body}
       </p>
-    </div>
-    `
-  ).join('');
+    </div>`
+    
 }
 
-main();
+renderPosts();
